4bccb87aea43ec59fc8a8d703b6d4664d95f5d90
Sally Clorinda <sallyclorinda20@gmail.com>
2023-03-09 23:59:39 +0700

    "mengubah file kuas.py"

--- luas.py
+++ luas.py
@@ -1,11 +1,16 @@
 print("=== Program menghitung luas persegi panjang ===")
-panjang= input("masukkan panjang: ")
-lebar= input("masukkan lebar: ")
-luas = int(panjang) * int(lebar)
+panjang = input("masukkan panjang: ")
+lebar   = input("masukkan lebar: ")
+luas    = int(panjang) * int(lebar)
 print(luas)
 print("luas persegi panjang adalah = ",(int(luas)))
 
+print("=== Program menghitung luas segitiga ===")
+alas            = input("masukkan alas segitiga: ")
+tinggi          = input("masukkan tinggi segitiga: ")
+luasSegitiga    = (int(alas) * int(tinggi))/2
+print(luasSegitiga)
+print("luas segitiga adalah = ",int(luasSegitiga))
 # codingan ini hanya untuk belajar
 # karena saya masih merasa perlu banyak belajar python language
 # created by sally clorinda
-       
